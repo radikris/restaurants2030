406e787627a6245201bbadd7341dd9d649261bf0
radikris <radi.kristof@gmail.com>
2021-10-05 23:51:54 +0200

    implemented basic carousel and dismisseable card

--- src/pages/waiter/waiter_page.tsx
+++ src/pages/waiter/waiter_page.tsx
@@ -1,9 +1,141 @@
-import React from 'react'
+import React from "react";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import {
+  Image,
+  VStack,
+  HStack,
+  Text,
+  Box,
+  Flex,
+  Center,
+  Square,
+} from "@chakra-ui/react";
+
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    paritialVisibilityGutter: 60,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    paritialVisibilityGutter: 50,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    paritialVisibilityGutter: 30,
+  },
+};
+const images = [
+  "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+];
+
+const leadingActions = () => (
+  <LeadingActions>
+    <SwipeAction onClick={() => console.info("swipe action triggered")}>
+      Action name
+    </SwipeAction>
+  </LeadingActions>
+);
+
+const trailingActions = () => (
+  <TrailingActions>
+    <SwipeAction
+      destructive={true}
+      onClick={() => console.info("swipe action triggered")}
+    >
+      Delete
+    </SwipeAction>
+  </TrailingActions>
+);
+
+const ALL_ORDER = [
+  {
+    name: "alma",
+  },
+  {
+    name: "korte",
+  },
+  {
+    name: "szilva",
+  },
+  {
+    name: "eper",
+  },
+];
 
 export default function WaiterPage() {
-    return (
-        <div>
-            THIS IS FOR THE WAITER
-        </div>
-    )
+  return (
+    <div>
+      <Carousel ssr partialVisbile responsive={responsive}>
+        {images.map((image, index) => {
+          return (
+            <Box flex="1">
+              <Flex color="white">
+                <VStack bg="white">
+                  <Text color="black">ASZTAL {index + 1}</Text>
+
+                  <Box bg="pink" width="300px">
+                    <Center>
+                      <VStack p="1.5">
+                        <Text>RENDELESEK</Text>
+                        {ALL_ORDER.map((order) => (
+                          <SwipeableList>
+                            <SwipeableListItem
+                              leadingActions={leadingActions()}
+                              trailingActions={trailingActions()}
+                            >
+                              <Text>{order.name}</Text>
+                            </SwipeableListItem>
+                          </SwipeableList>
+                        ))}
+                      </VStack>
+                    </Center>
+                  </Box>
+                  <Box bg="green" width="300px">
+                    <Center>
+                      <Text>FELSZOLGALT ETELEK</Text>
+                    </Center>
+                  </Box>
+                </VStack>
+              </Flex>
+            </Box>
+          );
+        })}
+      </Carousel>
+      <Box width="150px" bg="green.300" height="100%">
+        ALASDASLASFNFASBASFASK
+      </Box>
+    </div>
+  );
 }
